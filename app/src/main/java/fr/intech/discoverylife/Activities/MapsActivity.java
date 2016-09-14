d251166aa9d37809b2269647d7a55b5f593666ae
fcuchot <cuchot@intechinfo.fr>
2016-09-14 17:28:23 +0200

    Display Map

--- app/src/main/java/fr/intech/discoverylife/Activities/MapsActivity.java
+++ app/src/main/java/fr/intech/discoverylife/Activities/MapsActivity.java
@@ -47,6 +47,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
