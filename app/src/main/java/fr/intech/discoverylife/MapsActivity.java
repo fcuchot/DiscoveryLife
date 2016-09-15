4486ffa3618b0a91f59d7a02de9aadefd13e19cf
fcuchot <cuchot@intechinfo.fr>
2016-09-15 14:51:48 +0200

    Pairs marker + allow geo permission

--- app/src/main/java/fr/intech/discoverylife/MapsActivity.java
+++ app/src/main/java/fr/intech/discoverylife/MapsActivity.java
@@ -22,8 +22,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(48.866667, 2.333333);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Paris"));
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
